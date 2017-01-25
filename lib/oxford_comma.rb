cda80c378421c2931179a71dded0d5652d81681e
Edson Mendieta <emendietaj@gmail.com>
2017-01-25 02:17:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+    newArray = []
+    if array.size < 2
+        return array.join("")
+    elsif array.size == 2
+        return array.join(" and ")
+    else
+        counter = 0
+        array.each do |item|
+            if counter != array.size - 1
+                newArray << item
+                counter += 1
+            else
+                newArray << "and #{item}"
+            end
+        end
+        return newArray.join(", ")
+    end
 end
