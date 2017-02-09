a30cabba7b4406b695a6843e5c7aa2118a15bc37
Hiram Cruz <hiram0101@gmail.com>
2017-02-08 22:47:10 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+	case array.size
+		when 1
+			array.first
+		when 2
+			array.join(" and ")
+		else
+			last = array.pop
+			array.join(", ") + ", and #{last}"
+		end
 end
