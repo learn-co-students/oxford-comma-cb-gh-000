7305611ccf5c117a1880d9fed52b35a4f6d59630
Adrian Daras <getpingui@gmail.com>
2017-03-15 17:14:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    last = array.last
+    array.pop
+    new_array = []
+    new_array << array.join(", ")
+    new_array << ", and "
+    new_array << last
+    return new_array.join("")
+  end
 end
