55369d85d50a4b7e82bada4355ddc7dd52aae0dd
Robert Wayne <robertjwayne@ucla.edu>
2017-04-11 17:56:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case elements = array.size
+  when 1
+    array[0].to_s
+  when 2
+    array.join(" and ")
+  else
+    array[-1] = "and #{array[-1].to_s}"
+    array.join(", ")
+  end
 end
