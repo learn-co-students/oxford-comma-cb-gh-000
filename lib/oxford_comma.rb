529093ba7c95871939ad2aa4b216e63aadc1de26
Edmundo Elizondo <edmundo.geg@hotmail.com>
2017-05-24 21:17:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.first
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size > 2
+    array[array.size - 1] = "and #{array.last}"
+    array.join(", ")
+  end
 end
