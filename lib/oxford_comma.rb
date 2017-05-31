16dc83fdafbb502badca7e54b482a87b8ade37b9
Tuguldur Baigalmaa <philoniare@gmail.com>
2017-05-31 22:21:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.first
+  elsif array.size == 2
+    return array.first + " and " + array.last
+  else
+    return array[0, array.size-1].join(", ") + ", and " + array.last
+  end
 end
