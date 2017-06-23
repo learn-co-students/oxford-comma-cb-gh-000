01c8c5bf5947fe818638cb8927c9b2db0c457685
Esphas Kueen <esphas@hotmail.com>
2017-06-23 03:59:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.size > 2
+    array[0, array.size - 1].join(', ')+', and '+array.last
+  elsif array.size > 1
+    array.join ' and '
+  else
+    array[0] || ''
+  end
 end
