a63665b8996379cf55e1f678cb03b836372cccbb
Daan Schutte <daanskitte@gmail.com>
2017-06-29 18:58:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
+#the array `["fiddleheads","okra","kohlrabi"]` should get converted to the string `"fiddleheads, okra, and kohlrabi"`
 def oxford_comma(array)
-
+  result = ""
+  if array.length == 1
+    result = array[0]
+  elsif array.length == 2
+    result = array.join(" and ")
+  elsif array.length >= 2
+    last = array.pop
+    result = array.join(", ")
+    result << ", and " << last
+  end
+  result
 end
