c0001582452f3f092a36068e23a419a4dc824bd7
Anshul Chauhan <anshulchauhan007@gmail.com>
2017-07-06 06:34:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  i = 0
+  retval = ""
+  if array.length == 2
+    retval += "#{array[0]} and #{array[1]}"
+    return retval
+  end
+  while i < array.length do
+    if i > 0
+      retval += ", "
+      if i == array.length - 1
+        retval += "and "
+      end
+    end
+    retval += array[i]
+    i += 1
+  end
+  return retval
 end
