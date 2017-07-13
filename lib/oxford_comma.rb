40f17fc9b7ad109bba243e75241a6128e424caff
Jade Anderson <enighter@gmail.com>
2017-07-13 09:42:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    last_item = array.pop
+    message = array.join(", ")
+    message << ", and #{last_item}"
+    return message
+  end
 end
