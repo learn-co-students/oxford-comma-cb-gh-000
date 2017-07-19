144099cd534a1011debe0fd477658efc3d184357
Abhishek Chaudhary <acechd97@gmail.com>
2017-07-19 20:16:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.size == 1
+    array[0]
+  else
+    array[array.size - 1] = "and " + array[array.size - 1]
+    string = array.join(", ")
+    string
+  end
 end
