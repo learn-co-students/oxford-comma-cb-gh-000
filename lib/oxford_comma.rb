545299c386f73e766074a4ce551573a821d019ef
Steven Lai <slioweng@gmail.com>
2017-08-20 07:20:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
 
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join " and "
+  else
+    el = "and #{array[-1]}"
+    array.pop()
+    array << el
+    str1 = array.join ", "
+  end
 end
