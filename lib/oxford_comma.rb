d0227c271a7557cd019ed55550aba5133d219ba4
Nandan M <nandanm98@gmail.com>
2017-08-26 01:50:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  string=""
+  counter=0;
+  if array.length==1
+    return array.first
+  end
+  if array.length==2
+    return "#{array.first} #{"and"} #{array.last}"
+  end
+  while counter<array.length-1 do
+    string<<"#{array[counter]}#{", "}"
+    counter+=1
+  end
+  string<< "#{"and "}#{array[-1]}"
 end
