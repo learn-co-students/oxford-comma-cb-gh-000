3d9c94068dbe69c076265c0854455546211eb1fa
Jhondee Diaz <jhondeediaz@gmail.com>
2017-09-11 06:05:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  case array.size
+    when 1
+      array[0]
+    when 2
+      return "#{array[0]} and #{array[1]}"
+    else
+      array[-1] = "and #{array[-1]}"
+      newArray = array.join(', ')
+      newArray
+  end
 end
