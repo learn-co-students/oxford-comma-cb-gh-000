6384030f65d96ea469b8f08bb24e94946072489d
Matt Hough <matt@hough.tv>
2017-09-25 11:52:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+require "pry"
 
+array = ["fiddleheads","okra","kohlrabi"]
+
+def oxford_comma(array)
+  if array.size > 2
+    final_word_size = array[-1].size + 1
+    new_string = array.join(", ")
+    new_string = new_string.insert(-final_word_size, "and ")
+    new_string
+  elsif array.size == 2
+    new_string = array.join(" and ")
+  else
+    new_string = array.join
+  end
 end
+
+oxford_comma(array)
