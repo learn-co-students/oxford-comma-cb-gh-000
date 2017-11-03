8942c30f486c15e894b2d6cd8925f5511c96164f
Carlos Rivas <carlosr1765@gmail.com>
2017-11-03 15:19:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array.length <= 2
+      array.join(" and ")
+    else
+      index = array[-1]
+      new = "and #{index}"
+      array.map! { |x| x == index ? new : x }
+      array.join(", ")
+    end
 end
