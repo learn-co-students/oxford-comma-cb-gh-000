bd6a41e2aaf53c692597fdef02ff2cd0dc6bd7ef
Francesca Pietracaprina <fpietracaprina@gmail.com>
2017-11-14 23:07:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  arrayWithCommas=[]
+  arrayLast=array.pop
+  if array.length > 1
+    array.each do |el|
+      arrayWithCommas<<el<<", "
+    end
+  else
+    array[0] ? arrayWithCommas<<array[0]<<" " : nil
+  end
+  arrayWithCommas[-1] ? arrayWithCommas[-1]<<"and " : nil
+  arrayWithCommas<<arrayLast
+  arrayWithCommas.join
 end
