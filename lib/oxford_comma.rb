9cffe72e1e0780eb909ac7685bd24426db825454
Anthony Aleksejev <anthony.asdf@gmail.com>
2017-12-23 21:29:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.count == 2
+    x = array.join(" and ")
+  elsif array.count == 1
+    x = array.join
+  else
+    last = array.last
+    array.pop
+    x = array.join(", ")
+    x<<", and #{last}"
+  end
+  return x
 end
