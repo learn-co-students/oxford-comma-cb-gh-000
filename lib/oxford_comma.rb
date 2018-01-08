3eee4c9b5f74d84fe3d8fb0814992c436b1499f8
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-08 22:40:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join
+  elsif array.length == 2 
+    array.join(" and ")
+  elsif array.length == 3 
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else array.length > 3
+    mac = array.last
+    array.pop
+    array.join(", ") << ", and #{"#{mac}"}"
+  end
 end
+puts oxford_comma([1,2,3,4])
