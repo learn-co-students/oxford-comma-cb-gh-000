a3baf5e9d179433e177184246c8b8826b32292e3
Kyle Harris <k_harris95@live.com>
2018-01-17 21:43:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array[0]
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    sentence = array[0, array.size-1].join(", ")
+    sentence << ", and #{array[-1]}"
+  end
 end
