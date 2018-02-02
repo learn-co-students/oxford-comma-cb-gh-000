ddb632fde029ef80012ff5e3db960206665bf984
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-02-02 05:40:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
 
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    last_array = array.pop
+    message = array.join(", ")
+    message << " and #{last_array}"
+  else
+    last_array = array.pop
+    message = array.join(", ")
+    message << ", and #{last_array}"
+  end
 end
