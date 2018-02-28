9462a9bd393fe434b82f4dbf3dbd97bcd1533c5f
Harsh Vardhan <thvardhan.god@gmail.com>
2018-02-28 16:17:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+    if array.size == 2
+      return array.join " and "
+    elsif array.size == 1
+      array.join ","
+    else
+      return [array[0...-1].join(", "), array.last].join(", and ")
+    end
 end
