c8d0d5f93dfd75846dfeec627292cceb5e9f3654
norman lewis <takialog@gmail.com>
2018-02-28 06:13:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
 
+require 'pry'
+
+def oxford_comma(array)
+ case
+ when array.size == 1
+    array[0]
+  when array.size == 2
+    array[array.size - 1] = " and " + array.last
+    array.join()
+  when array.size == 3
+    array[array.size - 1] = "and " + array.last
+    array.join(', ')
+  when array.size > 3
+    array[array.size - 1] = "and " + array.last
+    array.join(', ')
+  end
 end
