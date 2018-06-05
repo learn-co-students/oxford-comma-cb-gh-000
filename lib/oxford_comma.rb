ddb2ca01c6b473a5b461d9e502d1d11772f07644
VISHAL RAJ <sunilkroy1973@gmail.com>
2018-06-05 13:45:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 2 
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length == 1 
+    return "#{array[0]}"
+  else 
+    last = array.pop
+    array.push("and")
+    string = array.join(", ")
+    string + " #{last}"
+  end 
 end
+
+# puts oxford_comma(["fiddleheads","okra","kohlrabi"])
+# p oxford_comma(["fiddleheads","okra","kohlrabi"])
