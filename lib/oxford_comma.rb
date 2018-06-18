6eece25ce8003a934fa6dd0bb5248f6e757ab4db
Krishan Jethwa <krishan.jethwa@student.manchester.ac.uk>
2018-06-18 21:51:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,29 @@
 def oxford_comma(array)
-
+  arr = []
+  counter = 1
+  if array.size == 1
+    return array.join()
+  elsif array.size == 2
+    array.each do |word|
+    if counter == array.size
+      arr << "and " + word
+      counter +=1
+      return arr.join()
+    else
+      arr << word + " "
+      counter += 1
+    end
+  end
+  else
+    array.each do |word|
+    if counter == array.size
+      arr << "and " + word
+      counter +=1
+    else
+      arr << word + ", "
+      counter += 1
+    end
+  end
+  arr.join()
+end
 end
