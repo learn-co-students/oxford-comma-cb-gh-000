958c66f5955a8bdda8670ebf58c36c090155a163
Quinn Dizon <qdizon@gmail.com>
2018-07-16 18:27:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+arr = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
 
+def oxford_comma(array)
+  if array.size > 2
+    array[-1] = "and #{array.last}"
+    array.join(", ")
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    return array.join
+  end
 end
+
+puts oxford_comma(arr)
