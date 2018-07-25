6d1af1f65bacdac83ce065c8c5735c0eaae236e2
Koy Saeteurn <kosatun@aol.com>
2018-07-25 23:33:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+      array.join("")
+  elsif array.length == 2 
+      two = array.join(" and ")
+       two
+  elsif array.length == 3
+      three =  array[0..-2].join(", ") + "," + " and " + array[-1]
+      three
+  elsif array.length > 3
+      more = array[0..-2].join(", ") + "," + " and " + array[-1]
+      more
+  end
 end
