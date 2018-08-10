158bbccd0a181dfba9f7f6bf1110a7ec6e27a8a6
Shane Glass <shaneryanglass88@gmail.com>
2018-08-10 22:03:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+ output = ""
+ if array.length == 2 
+   return array[0] + " and " + array[1]
+ end
+ while array.length > 0 do
+   if array.length >= 3 
+     output << " " + array.shift + ","
+   elsif array.length == 2 
+    output << " " + array.shift + ", and "
+  else
+    output << array.shift
+   end
+  end
+  return output.strip
 end
