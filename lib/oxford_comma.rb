bafe15a1d136fbb779d607e9359e1b610bef6254
Hamza Hedfi <h.hedfi@outlook.com>
2018-09-27 09:35:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.size
+    when 1
+      array.join
+    when 2
+      array.join(" and ")
+    else
+      last_item_in_array = array.pop
+      array.join(", ") << ", and " << last_item_in_array
+    end
 end
