e4635db91727af43b36e06ade1615e1732d630e4
Jakob Baumgartner <jakob.baumgartner@hotmail.com>
2018-10-10 15:30:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,30 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if(array.length == 1)
+    return array.join
+  end
+
+  if(array.length == 2)
+    return array.join(" and ")
+  end
+
+  novarray = []
+  i=0
+
+  while (i<array.length-1) do
+    novarray << array[i]
+    i+=1
+  end
+
+  evennovejsiarray = novarray.join(", ")
+  a0 = []
+
+  a0 << evennovejsiarray
+  a1 = array.pop
+  a0 << a1
+
+  a0 = a0.join(", and ")
+
+  return a0
 end
