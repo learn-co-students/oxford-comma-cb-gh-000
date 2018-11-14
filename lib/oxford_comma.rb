f4ef74d14812ef71ae17e8e96e311725976e5662
Carter Van Ekeren <vanek015@umn.edu>
2018-11-14 19:21:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    result = array[0]
+  elsif array.count == 2
+    result = "#{array[0]} and #{array[1]}"
+  else
+    result = ""
+    array.each {|element|
+      if element == array.last
+        result << "and #{element}"
+      else
+        result << "#{element}, "
+      end
+      }
+  end
+  result
 end
