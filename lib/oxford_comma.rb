c0d2ad5e3fc79dd51447e9b80c3883ec800656c3
Joseph Corona <josed.hc96@gmail.com>
2018-11-14 05:10:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  last = array.pop
+  ret = array.join(", ")
+  if (array.length > 1) then
+    ret += ", and #{last}"
+  elsif array.length == 1
+    ret += " and #{last}"
+  else
+    ret += last
+  end
 end
