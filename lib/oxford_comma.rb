b813a6a4b6e4322c91c2d28a58a658def5eee9f5
khalid Lamsadi <khalid.lamsadi@outlook.fr>
2018-12-14 22:31:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    #{}"#{array[0]} and #{array[1]}"
+    array.join(' and ')
+  else
+    last = array.pop
+    array.join(', ')
+    "#{array.join(', ')}, and #{last}"
+  end
 end
