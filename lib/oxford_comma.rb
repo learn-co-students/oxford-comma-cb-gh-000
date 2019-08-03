d123c242eae0e0d5e9ba1b645e94a1b402126beb
Logan Stone <logan.stone20@gmail.com>
2019-08-03 05:53:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  str = array.join(", ")
+  word = array.last
+  if array.size > 2
+    str = array.join(", ")
+    str.insert(str.index(word), "and ")
+  elsif array.size == 2
+    str = array.join(" and ")
+  else
+    str
+  end
+
 
 end
