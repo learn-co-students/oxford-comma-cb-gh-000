82a2d2785029b9b3bd21651804ccff814f1df617
Joshua Shewmaker <joshuashewmaker@gmail.com>
2019-08-07 21:17:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
-
+  sentence = ""
+  if array.size == 1
+    sentence << array[0]
+    return sentence
+  elsif array.size == 2
+    sentence << array[0]
+    sentence << " and "
+    sentence << array[1]
+    return sentence
+  end
+  count = array.size
+  array.each do |word|
+    if count == 1
+      sentence << "and " + word
+    else
+      sentence << word + ", "
+    end
+    count -= 1
+  end
+  return sentence
 end
