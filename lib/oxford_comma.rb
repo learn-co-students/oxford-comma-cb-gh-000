223e2dca11ea538ebfcb301be1a1f2c5d3a96086
Edgar V <evenegas9@ucmerced.edu>
2019-09-07 23:02:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-
+  if array.size < 2
+    return array.join
+  else
+    last = array.pop
+    return array.join(", ") + (array.size == 1 ? "" : ",") + " and #{last}"
+  end
 end
