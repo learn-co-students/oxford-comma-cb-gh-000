e220bca2d4eb5440f1b45887db66c99b845e9ede
Nikesh Kumar <nikeplusdash@gmail.com>
2019-12-12 07:44:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    pop = array.pop
+    x = array.join(", ")
+    x += ", and "+pop
+    x
+  end
 end
