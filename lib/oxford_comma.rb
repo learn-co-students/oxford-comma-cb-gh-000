4a4a7a6494a66f58d47d888101322eb195927a5a
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-08 19:51:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size<=2
+    ch=array.join(" and ")
+  else
+    ch=array.join(", ")
+    ar2=ch.split(", #{array[-1]}")
+    ch=ar2.join(", ")
+    ch<<(", and #{array[-1]}")
+  end
+  return  ch
 end
